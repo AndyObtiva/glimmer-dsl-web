ed8901415d1f814f4e632264f2a766f132814150
Andy Maleh <andy.am@gmail.com>
2024-01-04 17:00:23 -0500

    Complete New Hello, Component! Sample: `require glimmer-dsl-web/samples/hello/hello_component`

--- lib/glimmer-dsl-web/samples/hello/hello_component.rb
+++ lib/glimmer-dsl-web/samples/hello/hello_component.rb
@@ -21,7 +21,7 @@
 
 require 'glimmer-dsl-web'
 
-Address = Struct.new(:street, :street2, :city, :state, :zip_code, :billing_and_shipping, keyword_init: true) do
+Address = Struct.new(:full_name, :street, :street2, :city, :state, :zip_code, keyword_init: true) do
   STATES = {
     "AK"=>"Alaska",
     "AL"=>"Alabama",
@@ -89,43 +89,56 @@ Address = Struct.new(:street, :street2, :city, :state, :zip_code, :billing_and_s
   end
 
   def summary
-    string_attributes = to_h.except(:billing_and_shipping)
-    summary = string_attributes.values.map(&:to_s).reject(&:empty?).join(', ')
-    summary += " (Billing & Shipping)" if billing_and_shipping
-    summary
+    to_h.values.map(&:to_s).reject(&:empty?).join(', ')
   end
 end
 
+# AddressForm Glimmer Web Component (View component)
+#
+# Including Glimmer::Web::Component makes this class a View component and automatically
+# generates a new Glimmer GUI DSL keyword that matches the lowercase underscored version
+# of the name of the class. AddressForm generates address_form keyword, which can be used
+# elsewhere in Glimmer GUI DSL code as done inside AddressPage below.
 class AddressForm
   include Glimmer::Web::Component
   
-  before_render do
-    @address = Address.new(
-      street: '123 Main St',
-      street2: 'Apartment 3C, 2nd door to the right',
-      city: 'San Diego',
-      state: 'California',
-      zip_code: '91911',
-      billing_and_shipping: true,
-    )
-  end
+  option :address
   
+  # Optionally, you can execute code before rendering markup.
+  # This is useful for pre-setup of variables (e.g. Models) that you would use in the markup.
+  #
+  # before_render do
+  # end
+  
+  # Optionally, you can execute code after rendering markup.
+  # This is useful for post-setup of extra Model listeners that would interact with the
+  # markup elements and expect them to be rendered already.
+  #
+  # after_render do
+  # end
+  
+  # markup block provides the content of the
   markup {
     div {
       div(style: 'display: grid; grid-auto-columns: 80px 260px;') { |address_div|
-        label('Street: ', for: 'street-field')
+        label('Full Name: ', for: 'full-name-field')
+        input(id: 'full-name-field') {
+          value <=> [address, :full_name]
+        }
+        
+        @somelabel = label('Street: ', for: 'street-field')
         input(id: 'street-field') {
-          value <=> [@address, :street]
+          value <=> [address, :street]
         }
         
         label('Street 2: ', for: 'street2-field')
         textarea(id: 'street2-field') {
-          value <=> [@address, :street2]
+          value <=> [address, :street2]
         }
         
         label('City: ', for: 'city-field')
         input(id: 'city-field') {
-          value <=> [@address, :city]
+          value <=> [address, :city]
         }
         
         label('State: ', for: 'state-field')
@@ -134,23 +147,14 @@ class AddressForm
             option(value: state_code) { state }
           end
 
-          value <=> [@address, :state_code]
+          value <=> [address, :state_code]
         }
         
         label('Zip Code: ', for: 'zip-code-field')
         input(id: 'zip-code-field', type: 'number', min: '0', max: '99999') {
-          value <=> [@address, :zip_code,
+          value <=> [address, :zip_code,
                       on_write: :to_s,
                     ]
-        }
-        
-        div(style: 'grid-column: 1 / span 2') {
-          input(id: 'billing-and-shipping-field', type: 'checkbox') {
-            checked <=> [@address, :billing_and_shipping]
-          }
-          label(for: 'billing-and-shipping-field') {
-            'Use this address for both Billing & Shipping'
-          }
         }
         
         style {
@@ -166,16 +170,54 @@ class AddressForm
       }
       
       div(style: 'margin: 5px') {
-        inner_text <= [@address, :summary,
-                        computed_by: @address.members + ['state_code'],
+        inner_text <= [address, :summary,
+                        computed_by: address.members + ['state_code'],
                       ]
       }
     }
   }
 end
 
-# TODO add multiple addresses
+# AddressPage Glimmer Web Component (View component)
+#
+# This View component represents the main page being rendered,
+# as done by its `render` class method below
+class AddressPage
+  include Glimmer::Web::Component
+  
+  before_render do
+    @shipping_address = Address.new(
+      full_name: 'Johnny Doe',
+      street: '3922 Park Ave',
+      street2: 'PO BOX 8382',
+      city: 'San Diego',
+      state: 'California',
+      zip_code: '91913',
+    )
+    @billing_address = Address.new(
+      full_name: 'John C Doe',
+      street: '123 Main St',
+      street2: 'Apartment 3C',
+      city: 'San Diego',
+      state: 'California',
+      zip_code: '91911',
+    )
+  end
+  
+  markup {
+    div {
+      h1('Shipping Address')
+      
+      address_form(address: @shipping_address)
+      
+      h1('Billing Address')
+      
+      address_form(address: @billing_address)
+    }
+  }
+end
 
 Document.ready? do
-  AddressForm.render
+  # renders a top-level (root) AddressPage component
+  AddressPage.render
 end
