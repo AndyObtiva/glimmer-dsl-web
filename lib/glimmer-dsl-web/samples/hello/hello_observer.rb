13e7776069f1fe6a007ed7844d4798c54118cf40
Andy Maleh <andy.am@gmail.com>
2026-08-23 20:28:11 -0400

    Refactor Hello, Observer! & Hello, Observer Data-Binding! to make them safer for use in the sample selector app by only defining constants if not defined already

--- lib/glimmer-dsl-web/samples/hello/hello_observer.rb
+++ lib/glimmer-dsl-web/samples/hello/hello_observer.rb
@@ -1,55 +1,59 @@
 require 'glimmer-dsl-web'
 
-class NumberHolder
-  attr_accessor :number
-  
-  def initialize
-    self.number = 50
+unless Object.const_defined?(:NumberHolder) # this is only needed in the sample app, not in real apps
+  class NumberHolder
+    attr_accessor :number
+    
+    def initialize
+      self.number = 50
+    end
   end
 end
 
-class HelloObserver
-  include Glimmer::Web::Component
-  
-  before_render do
-    @number_holder = NumberHolder.new
-  end
-  
-  after_render do
-    @number_input.value = @number_holder.number
-    @range_input.value = @number_holder.number
+unless Object.const_defined?(:HelloObserver) # this is only needed in the sample app, not in real apps
+  class HelloObserver
+    include Glimmer::Web::Component
     
-    # Observe Model attribute @number_holder.number for changes and update View elements.
-    # Observer is automatically cleaned up when `remove` method is called on rendered
-    # HelloObserver web component or its top-level markup element (div)
-    observe(@number_holder, :number) do
-      number_string = @number_holder.number.to_s
-      @number_input.value = number_string unless @number_input.value == number_string
-      @range_input.value = number_string unless @range_input.value == number_string
+    before_render do
+      @number_holder = NumberHolder.new
     end
-    # Bidirectional Data-Binding does the same thing automatically as per alternative sample: Hello, Observer (Data-Binding)!
-  end
-  
-  markup {
-    div {
+    
+    after_render do
+      @number_input.value = @number_holder.number
+      @range_input.value = @number_holder.number
+      
+      # Observe Model attribute @number_holder.number for changes and update View elements.
+      # Observer is automatically cleaned up when `remove` method is called on rendered
+      # HelloObserver web component or its top-level markup element (div)
+      observe(@number_holder, :number) do
+        number_string = @number_holder.number.to_s
+        @number_input.value = number_string unless @number_input.value == number_string
+        @range_input.value = number_string unless @range_input.value == number_string
+      end
+      # Bidirectional Data-Binding does the same thing automatically as per alternative sample: Hello, Observer (Data-Binding)!
+    end
+    
+    markup {
       div {
-        @number_input = input(type: 'number', min: 0, max: 100) {
-          # oninput listener (observer) updates Model attribute @number_holder.number
-          oninput do
-            @number_holder.number = @number_input.value.to_i
-          end
+        div {
+          @number_input = input(type: 'number', min: 0, max: 100) {
+            # oninput listener (observer) updates Model attribute @number_holder.number
+            oninput do
+              @number_holder.number = @number_input.value.to_i
+            end
+          }
         }
-      }
-      div {
-        @range_input = input(type: 'range', min: 0, max: 100) {
-          # oninput listener (observer) updates Model attribute @number_holder.number
-          oninput do
-            @number_holder.number = @range_input.value.to_i
-          end
+        div {
+          @range_input = input(type: 'range', min: 0, max: 100) {
+            # oninput listener (observer) updates Model attribute @number_holder.number
+            oninput do
+              @number_holder.number = @range_input.value.to_i
+            end
+          }
         }
       }
     }
-  }
+  end
 end
 
 Document.ready? do
--- lib/glimmer-dsl-web/samples/hello/hello_observer_data_binding.rb
+++ lib/glimmer-dsl-web/samples/hello/hello_observer_data_binding.rb
@@ -1,34 +1,38 @@
 require 'glimmer-dsl-web'
 
-class NumberHolder
-  attr_accessor :number
-  
-  def initialize
-    self.number = 50
+unless Object.const_defined?(:NumberHolder) # this is only needed in the sample app, not in real apps
+  class NumberHolder
+    attr_accessor :number
+    
+    def initialize
+      self.number = 50
+    end
   end
 end
 
-class HelloObserverDataBinding
-  include Glimmer::Web::Component
-  
-  before_render do
-    @number_holder = NumberHolder.new
-  end
-  
-  markup {
-    div {
+unless Object.const_defined?(:HelloObserverDataBinding) # this is only needed in the sample app, not in real apps
+  class HelloObserverDataBinding
+    include Glimmer::Web::Component
+    
+    before_render do
+      @number_holder = NumberHolder.new
+    end
+    
+    markup {
       div {
-        input(type: 'number', min: 0, max: 100) {
-          value <=> [@number_holder, :number]
+        div {
+          input(type: 'number', min: 0, max: 100) {
+            value <=> [@number_holder, :number]
+          }
         }
-      }
-      div {
-        input(type: 'range', min: 0, max: 100) {
-          value <=> [@number_holder, :number]
+        div {
+          input(type: 'range', min: 0, max: 100) {
+            value <=> [@number_holder, :number]
+          }
         }
       }
     }
-  }
+  end
 end
 
 Document.ready? do
