f3727e0c676aabf91f6a89879490041159cb77aa
Andy Maleh <andy.am@gmail.com>
2026-08-30 23:57:33 -0400

    Improvements to Hello, Mutation & Hello, Mutation Content Data-Binding! samples

--- lib/glimmer-dsl-web/samples/hello/hello_mutation.rb
+++ lib/glimmer-dsl-web/samples/hello/hello_mutation.rb
@@ -1,49 +1,51 @@
 require 'glimmer-dsl-web'
 
-class HelloMutation
-  include Glimmer::Web::Component
-  
-  markup {
-    div {
-      h1('Hello, Mutation!')
-      
-      p {
-        'Add items to a list via element DOM mutations.'
-      }
-      
-      div(class: 'actions') {
-        @input = input(placeholder: 'Enter list item content')
+unless Object.const_defined?(:HelloMutation) # this is only needed in sample selector app due to file reloading, but not in real apps.
+  class HelloMutation
+    include Glimmer::Web::Component
+    
+    markup {
+      div {
+        h1('Hello, Mutation!')
         
-        button('Append list item') {
-          onclick do
-            @list.append {
-              li { @input.value }
-            }
-            @input.value = ''
-            @input.focus
-          end
+        p {
+          'Add items to a list via element DOM mutations.'
         }
         
-        button('Prepend list item') {
-          onclick do
-            @list.prepend {
-              li { @input.value }
-            }
-            @input.value = ''
-            @input.focus
-          end
+        div(class: 'actions') {
+          @input = input(placeholder: 'Enter list item content')
+          
+          button('Append list item') {
+            onclick do
+              @list.append {
+                li { @input.value }
+              }
+              @input.value = ''
+              @input.focus
+            end
+          }
+          
+          button('Prepend list item') {
+            onclick do
+              @list.prepend {
+                li { @input.value }
+              }
+              @input.value = ''
+              @input.focus
+            end
+          }
         }
+        
+        @list = ul
       }
-      
-      @list = ul
     }
-  }
-  
-  style {
-    r('.actions input, .actions button') {
-      margin '10px 10px 10px 0'
+    
+    style {
+      r('.actions input, .actions button') {
+        margin '10px 10px 10px 0'
+      }
     }
-  }
+  end
 end
 
 Document.ready? do
--- lib/glimmer-dsl-web/samples/hello/hello_mutation_content_data_binding.rb
+++ lib/glimmer-dsl-web/samples/hello/hello_mutation_content_data_binding.rb
@@ -1,81 +1,85 @@
 require 'glimmer-dsl-web'
 
-class ListPresenter
-  attr_accessor :list, :new_list_item
-  
-  def initialize
-    @list = []
-    @new_list_item = ''
-  end
-  
-  def append_list_item
-    list.push(new_list_item)
-    clear_new_list_item
-  end
-  
-  def prepend_list_item
-    list.prepend(new_list_item)
-    clear_new_list_item
-  end
-  
-  def clear_new_list_item
-    self.new_list_item = ''
-  end
-  
-  def entering_new_list_item?
-    new_list_item.empty?
+unless Object.const_defined?(:ListPresenter) # this is only needed in sample selector app due to file reloading, but not in real apps.
+  class ListPresenter
+    attr_accessor :list, :new_list_item
+    
+    def initialize
+      @list = []
+      @new_list_item = ''
+    end
+    
+    def append_list_item
+      list.push(new_list_item)
+      clear_new_list_item
+    end
+    
+    def prepend_list_item
+      list.prepend(new_list_item)
+      clear_new_list_item
+    end
+    
+    def clear_new_list_item
+      self.new_list_item = ''
+    end
+    
+    def entering_new_list_item?
+      new_list_item.empty?
+    end
   end
 end
 
-class HelloMutationContentDataBinding
-  include Glimmer::Web::Component
-  
-  before_render do
-    @list_presenter = ListPresenter.new
+unless Object.const_defined?(:HelloMutationContentDataBinding) # this is only needed in sample selector app due to file reloading, but not in real apps.
+  class HelloMutationContentDataBinding
+    include Glimmer::Web::Component
+    
+    before_render do
+      @list_presenter = ListPresenter.new
+    end
+    
+    markup {
+      div {
+        h1('Hello, Mutation Content Data-Binding!')
+        
+        p {
+          'Add items to a list via content data-binding (which performs implicit DOM mutations).'
+        }
+        
+        div(class: 'actions') {
+          input(placeholder: 'Enter list item content') {
+            value <=> [@list_presenter, :new_list_item]
+            focused <= [@list_presenter, :entering_new_list_item?, computed_by: :new_list_item]
+          }
+          
+          button('Append list item') {
+            onclick do
+              @list_presenter.append_list_item
+            end
+          }
+          
+          button('Prepend list item') {
+            onclick do
+              @list_presenter.prepend_list_item
+            end
+          }
+        }
+        
+        ul {
+          content(@list_presenter, :list) {
+            @list_presenter.list.each do |list_item|
+              li { list_item }
+            end
+          }
+        }
+      }
+    }
+    
+    style {
+      r('.actions input, .actions button') {
+        margin '10px 10px 10px 0'
+      }
+    }
   end
-  
-  markup {
-    div {
-      h1('Hello, Mutation Content Data-Binding!')
-      
-      p {
-        'Add items to a list via content data-binding (which performs implicit DOM mutations).'
-      }
-      
-      div(class: 'actions') {
-        @input = input(placeholder: 'Enter list item content') {
-          value <=> [@list_presenter, :new_list_item]
-          focused <= [@list_presenter, :entering_new_list_item?, computed_by: :new_list_item]
-        }
-        
-        button('Append list item') {
-          onclick do
-            @list_presenter.append_list_item
-          end
-        }
-        
-        button('Prepend list item') {
-          onclick do
-            @list_presenter.prepend_list_item
-          end
-        }
-      }
-      
-      ul {
-        content(@list_presenter, :list) {
-          @list_presenter.list.each do |list_item|
-            li { list_item }
-          end
-        }
-      }
-    }
-  }
-  
-  style {
-    r('.actions input, .actions button') {
-      margin '10px 10px 10px 0'
-    }
-  }
 end
 
 Document.ready? do
